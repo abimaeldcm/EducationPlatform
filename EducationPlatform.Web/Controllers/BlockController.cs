3c251fc8ada451c8092f235bbec37f50fb9aed95
abima <abima@DESKTOP-FKO1IBA>
2024-04-23 02:51:23 -0300

    FRONT  - Iniciado os módulos

--- EducationPlatform.Web/Controllers/BlockController.cs
+++ EducationPlatform.Web/Controllers/BlockController.cs
@@ -1,12 +1,102 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EducationPlatform.Web.Domain.Entity;
+using EducationPlatform.Web.Filters;
+using EducationPlatform.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EducationPlatform.Web.Controllers
 {
+    [PaginaUsuarioLogado]
     public class BlockController : Controller
     {
-        public IActionResult Index()
+        private readonly ICRUD<BlockOutput, BlockInput> _BlockService;
+
+        public BlockController(ICRUD<BlockOutput, BlockInput> BlockService)
         {
+            _BlockService = BlockService;
+        }
+
+        public async Task<ActionResult<IEnumerable<BlockOutput>>> Index()
+        {
+            var BlocksDb = await _BlockService.BuscarTodos();
+
+            return View(BlocksDb);
+        }
+
+        public async Task<ActionResult> Details(int id)
+        {
+            return View(await _BlockService.BuscarPorId(id));
+        }
+
+        public async Task<ActionResult> Create()
+        {
+            /* ViewBag.BloodType = new SelectList(Enum.GetValues(typeof(ETBloodType)));
+             ViewBag.SpecialityMedical = new SelectList((await _SpecialityService.BuscarTodos()).OrderBy(s => s.MedicalSpeciality), "Id", "MedicalSpeciality");*/
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(BlockInput collection)
+        {
+            try
+            {
+                //ViewBag.SpecialityId = new SelectList((await _SpecialityService.BuscarTodos()).OrderBy(s => s.MedicalSpeciality), "Id", "Name");
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public async Task<ActionResult> Edit(int id)
+        {
+            //ViewBag.BloodType = new SelectList(Enum.GetValues(typeof(ETBloodType)));
+            //ViewBag.SpecialityMedical = new SelectList((await _SpecialityService.BuscarTodos()).OrderBy(s => s.MedicalSpeciality), "Id", "MedicalSpeciality");
+            var medicoDb = await _BlockService.BuscarPorId(id);
+            return View(medicoDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int id, BlockInput BlockEdit)
+        {
+            try
+            {
+                await _BlockService.Editar(id, BlockEdit);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        [PaginaUsuarioAdm]
+        public async Task<ActionResult> Delete(int id)
+        {
+            return View(await _BlockService.BuscarPorId(id));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [PaginaUsuarioAdm]
+        public async Task<ActionResult> DeleteExecute(int id)
+        {
+            try
+            {
+                var BlockDb = await _BlockService.BuscarPorId(id);
+                if (BlockDb != null)
+                {
+                    await _BlockService.Delete(id);
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
---
+++ EducationPlatform.Web/Services/BlockService.cs
@@ -0,0 +1,224 @@
+﻿using System.Text.Json;
+using System.Text;
+using System.Net.Http.Headers;
+using EducationPlatform.Web.Helper;
+using EducationPlatform.Web.Domain.Entity;
+using EducationPlatform.Web.Services.Interfaces;
+
+namespace EducationPlatform.Web.Services
+{
+    public class BlockService : ICRUD<BlockOutput, BlockInput>
+    {
+        private readonly IHttpClientFactory _ClientFactory;
+        private const string apiEndpoint = "api/Block";
+        private readonly JsonSerializerOptions _options;
+        private BlockOutput Block;
+        private IEnumerable<BlockOutput> Blocks;
+        private readonly ISessao _isessao;
+
+
+        public BlockService(IHttpClientFactory clientFactory, ISessao isessao)
+        {
+            _ClientFactory = clientFactory;
+            _options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            _isessao = isessao;
+        }
+
+        public async Task<BlockOutput> BuscarPorId(int id)
+        {
+            var client = _ClientFactory.CreateClient("EducationPlatform");
+
+            var sessaoBlock = _isessao.BuscarSessaoDoUsuario();
+
+            if (sessaoBlock != null && !string.IsNullOrEmpty(sessaoBlock.Token))
+            {
+                var tokenJwt = sessaoBlock.Token;
+
+                var requestUri = $"https://localhost:7018/api/Block/{id}";
+
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri(requestUri),
+                };
+
+                request.Headers.Add("Authorization", $"Bearer {tokenJwt}");
+
+                using (var response = await client.SendAsync(request))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var apiResponse = await response.Content.ReadAsStringAsync();
+                        Block = JsonSerializer.Deserialize<BlockOutput>(apiResponse, _options);
+                    }
+                }
+            }
+
+            return Block;
+        }
+
+        public async Task<IEnumerable<BlockOutput>> BuscarTodos()
+        {
+            var client = _ClientFactory.CreateClient("EducationPlatform");
+
+            var sessaoBlock = _isessao.BuscarSessaoDoUsuario();
+
+            IEnumerable<BlockOutput> Blocks = null;
+
+            if (sessaoBlock != null && !string.IsNullOrEmpty(sessaoBlock.Token))
+            {
+                var tokenJwt = sessaoBlock.Token;
+
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri("https://localhost:7018/api/Block"),
+                };
+
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", tokenJwt);
+
+                using (var response = await client.SendAsync(request))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var apiResponse = await response.Content.ReadAsStringAsync();
+                        Blocks = JsonSerializer.Deserialize<IEnumerable<BlockOutput>>(apiResponse, _options);
+                    }
+                }
+            }
+
+            return Blocks;
+        }
+
+        public async Task<IEnumerable<BlockOutput>> BuscarPorTexto(string termoPesquisa)
+        {
+            var client = _ClientFactory.CreateClient("EducationPlatform");
+
+            var sessaoBlock = _isessao.BuscarSessaoDoUsuario();
+
+            IEnumerable<BlockOutput> Blocks = null;
+
+            if (sessaoBlock != null && !string.IsNullOrEmpty(sessaoBlock.Token))
+            {
+                var tokenJwt = sessaoBlock.Token;
+
+                var requestUri = $"https://localhost:7018/api/Block/BuscarPorTexto/{termoPesquisa}";
+
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri(requestUri),
+                };
+
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", tokenJwt);
+
+                using (var response = await client.SendAsync(request))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var apiResponse = await response.Content.ReadAsStringAsync();
+                        Blocks = JsonSerializer.Deserialize<IEnumerable<BlockOutput>>(apiResponse, _options);
+                    }
+                }
+            }
+
+            return Blocks;
+        }
+
+        public async Task<BlockOutput> Cadastrar(BlockInput cadastrar)
+        {
+            var client = _ClientFactory.CreateClient("EducationPlatform");
+
+            var sessaoBlock = _isessao.BuscarSessaoDoUsuario();
+
+            if (sessaoBlock != null && !string.IsNullOrEmpty(sessaoBlock.Token))
+            {
+                var tokenJwt = sessaoBlock.Token;
+
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Post,
+                    RequestUri = new Uri("https://localhost:7018/api/Block"),
+                    Content = new StringContent(JsonSerializer.Serialize(cadastrar), Encoding.UTF8, "application/json")
+                };
+
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", tokenJwt);
+
+                using (var response = await client.SendAsync(request))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var apiResponse = await response.Content.ReadAsStringAsync();
+                        Block = JsonSerializer.Deserialize<BlockOutput>(apiResponse, _options);
+                    }
+                }
+            }
+
+            return Block;
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            var client = _ClientFactory.CreateClient("EducationPlatform");
+
+            var sessaoBlock = _isessao.BuscarSessaoDoUsuario();
+
+            if (sessaoBlock != null && !string.IsNullOrEmpty(sessaoBlock.Token))
+            {
+                var tokenJwt = sessaoBlock.Token;
+
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Delete,
+                    RequestUri = new Uri($"https://localhost:7018/api/Block/{id}"),
+                };
+
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", tokenJwt);
+
+                using (var response = await client.SendAsync(request))
+                {
+                    return response.IsSuccessStatusCode;
+                }
+            }
+
+            return false;
+        }
+
+        public async Task<object> Editar(int id, BlockInput editar)
+        {
+            var client = _ClientFactory.CreateClient("EducationPlatform");
+
+            var sessaoBlock = _isessao.BuscarSessaoDoUsuario();
+
+            if (sessaoBlock != null && !string.IsNullOrEmpty(sessaoBlock.Token))
+            {
+                var tokenJwt = sessaoBlock.Token;
+
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Put,
+                    RequestUri = new Uri($"https://localhost:7018/api/Block/{id}"),
+                    Content = new StringContent(JsonSerializer.Serialize(editar), Encoding.UTF8, "application/json")
+                };
+
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", tokenJwt);
+
+                using (var response = await client.SendAsync(request))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var apiResponse = await response.Content.ReadAsStringAsync();
+                        Block = JsonSerializer.Deserialize<BlockOutput>(apiResponse, _options);
+                    }
+                    else
+                    {
+                        throw new Exception(await response.Content.ReadAsStringAsync());
+                    }
+                }
+            }
+
+            return Block;
+        }
+
+    }
+}
